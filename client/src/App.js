592b4b205a962f161e12e53e688550e07e73d813
ALEJANDRO ZAMORA <33189440+AlejandroZam@users.noreply.github.com>
2019-12-08 22:29:14 -0500

    added ui elements and css style but please help
    i tried to add overlay on the menubutton in menubutton.css but it doesn't do anything literally nothing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import './App.css'
+import './App.css';
 import store from './store'
 import OpenStreetMap from './components/OpenStreetMap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import MenuButton from './MenuButton'
+import Menu from './Menu'
+
 class App extends Component {
+
+constructor(props,context){
+  super(props,context);
+  this.state = {
+    visible: false
+  };
+ 
+  this.handleMouseDown = this.handleMouseDown.bind(this);
+  this.toggleMenu = this.toggleMenu.bind(this);
+}
+
+handleMouseDown(e) {
+  this.toggleMenu();
+
+  console.log("clicked");
+  e.stopPropagation();
+}
+
+toggleMenu(){
+  this.setState({
+    visible: !this.state.visible
+  })
+}
 
   render () {
     return (
+
       <Provider store={ store }>
         <div className="App">
+
+        <MenuButton handleMouseDown={this.handleMouseDown}/>
+      <Menu handleMouseDown={this.handleMouseDown}
+      menuVisibility={this.state.visible}/>
           <OpenStreetMap/>
         </div>
       </Provider>
